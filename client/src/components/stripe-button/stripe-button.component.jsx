2efdc534064a5dec4c53a1ea7c89108e2eb2e8c8
Ravi <patel.ravi04@gmail.com>
2020-04-15 05:52:57 -0500

    minor fix and updated gitignore

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
-import { response } from 'express';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
